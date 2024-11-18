d59f50f14fcd2cec579be8a779442e98329eb9b1
phanduc172 <ph.duc172@gmail.com>
2024-11-18 14:46:09 +0700

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -58,15 +58,73 @@ let swiperPopular = new Swiper(".popular__container", {
 
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
-
+let mixerFeatured = mixitup('.featured__content', {
+    selectors: {
+        target: '.featured__card'
+    },
+    animation: {
+        duration: 300
+    }
+})
 
 /* Link active featured */
+const linkFeatured = document.querySelectorAll('.featured__item')
+
+function activeFeatured() {
+    linkFeatured.forEach(l => l.classList.remove('active-featured'))
+    this.classList.add('active-featured')
+}
+linkFeatured.forEach(l => l.addEventListener('click', activeFeatured))
 
 
 /*=============== SHOW SCROLL UP ===============*/
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up')
+    if (this.scrollY >= 350)
+        scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
 
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.pageYOffset
+    sections.forEach(current => {
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 58,
+            sectionId = current.getAttribute('id')
+
+        const links = document.querySelectorAll('.nav__menu a[href*=' + sectionId + ']');
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            links.forEach(link => link.classList.add('active-link'))
+        } else {
+            links.forEach(link => link.classList.remove('active-link'))
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay: 400,
+    reset: true
+})
+
+sr.reveal(`.home__title, .popular__container, .features__img, .featured__filtesrs`)
+sr.reveal(`.home__subtitle`, { delay: 500 })
+sr.reveal(`.home__elec`, { delay: 600 })
+sr.reveal(`.home__img`, { delay: 800 })
+sr.reveal(`.home__car-data`, { delay: 900, interval: 100, origin: 'bottom' })
+sr.reveal(`.home__button`, { delay: 1000, origin: 'bottom' })
+sr.reveal(`.about__group, .offer__data`, { origin: 'left' })
+sr.reveal(`.about__data, .offer__img`, { origin: 'right' })
+sr.reveal(`.features__map`, { delay: 600, origin: 'bottom' })
+sr.reveal(`.features__card`, { interval: 300 })
+sr.reveal(`.features__card, .logos__content, .footer__content`, { interval: 100 })
